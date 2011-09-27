28fee2f1aaa1e5a13f78d569f0d9cecdd6e0c339
Quasar Jarosz <quasar@ja.rosz.org>
2011-09-27 01:36:24 -0500

    update

---
+++ AtlasLoot.lua
@@ -0,0 +1,26 @@
+
+AtlasLootDB = {
+	["namespaces"] = {
+		["DefaultFrame"] = {
+			["profiles"] = {
+				["Deadset - Auchindoun"] = {
+					["instance"] = "Firelands",
+				},
+			},
+		},
+		["AtlasLootPanel"] = {
+		},
+		["WishList"] = {
+		},
+		["Filter"] = {
+		},
+	},
+	["showWarning"] = true,
+	["profileKeys"] = {
+		["Deadset - Auchindoun"] = "Deadset - Auchindoun",
+	},
+	["profiles"] = {
+		["Deadset - Auchindoun"] = {
+		},
+	},
+}
---
+++ GearScore.lua
@@ -0,0 +1,71 @@
+
+TenTonHammer_Settings = {
+	["minimap_button"] = 1,
+	["addonMode"] = 1,
+	["color"] = "Yellow",
+	["ExperienceTab"] = 1,
+	["anchor"] = "TOPRIGHT",
+	["inspect"] = 1,
+	["Theme"] = "Neon",
+	["usertips"] = 1,
+	["ratingOnTip"] = 0,
+	["collapse"] = 1,
+	["TalentTab"] = 1,
+	["DDL"] = 1,
+	["raidOnTip"] = 0,
+	["database"] = 1,
+	["target_inspect1"] = 1,
+	["scale"] = 5,
+	["chat"] = 1,
+	["GroupTab"] = 1,
+	["pveOnTip"] = 1,
+	["showOffSpec"] = 0,
+	["NewsTab"] = 1,
+	["REG"] = "us",
+	["EquipTab"] = 1,
+	["panel"] = 0,
+	["masterloot"] = 1,
+	["foobar"] = 0,
+	["button_hammer"] = 0,
+	["verbose"] = 1,
+	["ailOnTip"] = 1,
+	["quickrate"] = 1,
+	["PvPTab"] = 1,
+	["gearOnTip"] = 1,
+	["online"] = 1,
+	["update"] = 0,
+	["filter"] = 1,
+	["pvpOnTip"] = 0,
+	["itemtips"] = 1,
+	["delay"] = 500,
+	["SVR"] = "Auchindoun",
+}
+TenTonHammer_Database = {
+	["Kel'Thuzad"] = {
+		["Ahras"] = "201109270021:9524:83:7812:704:0:0:6:0:0:0",
+		["Poobeard"] = "201109270021:8172:93:8715:2697:0:0:1:0:0:0",
+	},
+	["Exodar"] = {
+		["Capps"] = "201109270026:8946:0:7327:0:0:0:2:0:0:0",
+	},
+	["Auchindoun"] = {
+		["Gust Soldier"] = "201109270026:0:1927:0:1068:0:0:1:0:0:0",
+		["Chukk"] = "201109270026:8329:0:7054:0:0:0:5:0:0:0",
+		["Snowguard"] = "201109231938:9964:4685:8157:5004:0:0:1:0:0:0",
+		["Sloane"] = "201109251356:9035:13:7640:1264:0:0:1:0:0:0",
+	},
+	["Version"] = 4400,
+}
+TenTonHammer_Elite = {
+	[795] = {
+		["ENCHANTS"] = {
+		},
+		["GEMS"] = {
+		},
+		["TALENTS"] = {
+		},
+	},
+}
+TenTonHammer_OnlineDatabase = nil
+TTHD = "@"
+TenTonHammer_News = nil
